16e05722b8493b866962fb633a56518d27cc8f82
Matt Pedersen <mpedersen2054@gmail.com>
2013-12-24 19:38:05 -0600

    fix false positive in person specs

--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -1,16 +1,22 @@
 require 'spec_helper'
 
 describe Person do
+  let(:person) do
+    Person.new(first_name: "Matt", last_name: "Pedersen")
+  end
+
   it 'should be valid' do
     person = Person.new(first_name: "Matt", last_name: "Pedersen")
     expect(person).to be_valid
   end
+
   it 'is invalid without a first name' do
-    person = Person.new(first_name: nil)
+    person.first_name = nil
     expect(person).to_not be_valid
   end
+
   it 'is invalid without a last name' do
-    person = Person.new(first_name: "Freg", last_name: nil)
+    person.last_name = nil
     expect(person).to_not be_valid
   end
 end
